2ed35b4a0c130ffcac8f132674ffbd15d7c7af08
xiaokc12 <2338028940@qq.com>
2025-04-29 01:59:11 +0800

    上传项目

---
+++ generate_dataset_json/generate_training_datasets_eye_class_specific.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# cls_ids=(0 1 2 3 4 5 6 7 8 9)
+cls_ids=(0 1 2)
+for cls_id in "${!cls_ids[@]}";do
+    python mvtec_3d_anomaly_eye_ad_class_specific.py --cls_id ${cls_ids[cls_id]}
+    wait
+done
